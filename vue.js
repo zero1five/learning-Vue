471d8584a4c7373a2a40188ec7d7cf3b26939bbe
zero1five <2642585002@qq.com>
2018-02-05 01:09:46 +0800

    整合Vue的知识点

---
+++ vue.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#demo',
+    data () {
+        return {
+            list: list
+        }
+    },
+    watch: {
+        list: function(){ /* list里的数据改变时就会触发这个函数 */
+            // coding   /* 浅监控 只能监控list对象下的一层 */
+        },
+        list: {
+            handler: function(){
+                // coding
+            },
+            deep: true   /* 深度监控 可以监控list对象下的所有 */
+        }
+    }
+
+})
